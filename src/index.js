03d4e1c84baa00be3610909557ea439f1fba3e20
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-01 09:19:36 +0200

    feat: add index with dummy details

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './assets/style.css';
+import Display from './modules/display';
+
+Display.initDisplay();
+Display.insertProject('TETS');
+Display.insertProject('TETS');
+Display.insertProject('TETS');
+Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
+Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
+Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
+Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
