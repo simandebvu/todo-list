7f4efb1d404aab3046901225f319c25f19b8f563
Shingirayi Innocent Mandebvu <shingim1@gmail.com>
2020-09-02 11:18:14 +0200

    feat: add user input forms

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/style.css';
+import 'bootstrap';
 import Display from './modules/display';
+import Project from './modules/Project';
+
 
 Display.initDisplay();
-Display.insertProject('TETS');
-Display.insertProject('TETS');
-Display.insertProject('TETS');
-Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
-Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
-Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
-Display.insertProjectTodo('projec', 'title', 'date', 'description', '');
+
+
+const formProject = document.querySelector('.add-project-form');
+formProject.onsubmit = (e) => {
+  e.preventDefault();
+  const {
+    projectName,
+  } = e.target.elements;
+  Display.insertProject(projectName.value);
+  Project.addProject(projectName.value);
+  e.target.reset();
+};
+
+const form = document.querySelector('#formToDo');
+form.onsubmit = (e) => {
+  e.preventDefault();
+  Display.closeTodoForm();
+  const {
+    title, description, date, priority,
+  } = e.target.elements;
+  Project.addTodo(
+    title.value,
+    description.value,
+    date.value,
+    priority.value,
+  );
+  e.target.reset();
+};
+Project.loadStoredProjects();
